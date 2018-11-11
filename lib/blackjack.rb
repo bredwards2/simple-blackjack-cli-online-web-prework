4de3e99e885e8fb36f3942f9ca0c0166d52ec0d7
Britny Edwards <bredwards2@gmail.com>
2018-11-11 23:33:07 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,7 +7,7 @@ def deal_card(1..11)
 end
 
 def display_card_total
-  # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -18,8 +18,8 @@ def get_user_input
   # code #get_user_input here
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
